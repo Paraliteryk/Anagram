aec3c81648bfea681c3a5a977602425ab81a6456
Paraliteryk <paraliteryk@gmail.com>
2016-11-04 10:59:32 +0100

    dodana baza slow oraz funkcja mieszajaca litery w slowie

---
+++ lab04/panel.php
@@ -0,0 +1,48 @@
+<?php
+
+	session_start();
+	
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8"/>
+	<title>Anagram</title>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+
+<body>
+<h1>Anagram</h1>
+<div id="gracz">
+<?php
+echo " Witaj ".$_SESSION['nick'];
+echo "<p> Twój ranking: ".$_SESSION['ranking'];
+echo "<p> Wygrane: 0";
+echo "<p> Przegrane: 0";
+echo "<p> Najlepszy wynik: 0 <p>";
+?>
+<a href="statystyki.php" target="_blank">Zobacz dokładne statystyki</a>
+<a href="zmiania.php" >Zmień dane</a>
+<a href="index.php" >Wyloguj się</a>
+<p><p>
+<a href="update.php"  >Wprowadź update do słownika</a>
+ </div>
+ 
+ <p>
+ <div id="trening">
+		<form action="trening.php">
+  <input type="submit" value="trening">
+  </form>
+
+ </div>
+<?php
+ if(isset($_SESSION['zmieniono']))
+	{
+		echo $_SESSION['zmieniono'];
+		unset($_SESSION['zmieniono']);
+	}
+?>
+</body>
+</html>
+ 
---
+++ lab04/trening.php
@@ -0,0 +1,69 @@
+﻿<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Anagram</title>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+
+<body>
+<h1>Anagram</h1>
+
+<?php
+		require_once "connect.php";
+		$idslowa=rand(1,100000);
+		$polaczenie = new mysqli($host,$db_user,$db_password,$db_name);
+		$rezultat=$polaczenie->query("SELECT slowo FROM slownik6 WHERE id='$idslowa'");
+		$ilosc=$rezultat->num_rows;
+
+			while($ilosc==0)
+			{
+				$idslowa=rand(1,100000);
+				$rezultat=$polaczenie->query("SELECT slowo FROM slownik6 WHERE id='$idslowa'");
+				$ilosc=$rezultat->num_rows;
+			}
+			$wiersz=$rezultat->fetch_assoc();
+			$poprawne=$wiersz['slowo'];
+			echo "<p> (".$poprawne.")</p>";
+			
+			$tab = array();
+			for($i = 0; $i < strlen($poprawne); $i++)
+					$tab[$i] = $poprawne[$i];
+		
+		
+			$tab2=array();
+            for($i=0;$i<strlen($poprawne);$i++)
+            {
+                $tab2[$i]=$tab[$i];
+            }
+            for($i=0;$i<strlen($poprawne);$i++)
+            {
+				$r=strlen($poprawne)-1;
+               $a=rand(0,$r);
+
+                  while($tab2[$a]=='1')
+                         $a=rand(0,$r);
+             
+                   $tab[$i]= $tab2[$a];
+                    $tab2[$a]='1';
+            }
+			for($i = 0; $i < strlen($poprawne); $i++)
+					echo "<span style=\"font-size: 40px;\"> $tab[$i] </span>";
+		
+?>	
+
+<form action="trening.php">
+  <input type="submit" value="następny">
+  </form>
+</body>
+</html>
+
+
+
+
